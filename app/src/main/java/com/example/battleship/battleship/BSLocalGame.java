433d2bf08bc8f8986d910ebc8321fbec1ccd2e1a
natek <klinen22@up.edu>
2020-03-20 16:12:34 -0700

    Added framework w/some extra

---
+++ app/src/main/java/com/example/battleship/battleship/BSLocalGame.java
@@ -0,0 +1,27 @@
+package com.example.battleship.battleship;
+
+import com.example.battleship.GameFramework.LocalGame;
+
+public class BSLocalGame extends LocalGame {
+    //Tag for logging
+    private static final String TAG = "BSLocalGame";
+    // the game's state
+    //protected BSState state;
+
+    // the marks for player 0 and player 1, respectively
+    private final static char[] mark = {'X','O'};
+
+    // the number of moves that have been played so far, used to
+    // determine whether the game is over
+    protected int moveCount;
+
+    public BSLocalGame() {
+
+        // perform superclass initialization
+        super();
+
+        // create a new, unfilled-in TTTState object
+        //state = new BSState();
+    }
+
+}
--- app/src/main/java/com/example/battleship/game/GameFramework/Game_Config.java
+++
@@ -1,4 +0,0 @@
-package com.example.battleship.game.GameFramework;
-
-public class Game_Config {
-}
