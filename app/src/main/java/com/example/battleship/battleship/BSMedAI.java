472451f7fcdc03461aec0eabe8cd81950f735ec0
Grant Nelson <nelsong22@up.edu>
2020-04-19 21:32:23 -0700

    added MedAI class (Finished?) needs testing [used switch cases for different AI choices

--- app/src/main/java/com/example/battleship/battleship/BSMedAI.java
+++ app/src/main/java/com/example/battleship/battleship/BSMedAI.java
@@ -22,6 +22,7 @@ public class BSMedAI extends GameComputerPlayer {
         } else {
             return;
         }
+
         if(((BSGameState) info).inGame == false){
             game.sendAction(new BSPlaceCPUShip(this, 1));
             // game.sendAction(new BSPlaceCPUShip(this, ((int)(Math.random()) * 19 + 1)));
@@ -29,27 +30,87 @@ public class BSMedAI extends GameComputerPlayer {
 
         int randX, randY;
         int medX, medY;
+        int prevX, prevY;
+
+
+
+
         do {
             randX = randomX();
             randY = randomY();
+
+            prevX = randX;
+            prevY = randY;
         } while (((BSGameState) info).humanPlayerBoard[randX][randY] == 1 ||
                 ((BSGameState) info).humanPlayerBoard[randX][randY] == 2);
-        do{
-
-        } while (((BSGameState) info).computerPlayerHits == ((BSGameState) info).computerPlayerHits + 1);
 
         sleep(.75);
 
         game.sendAction(new BSFire(this, randX, randY));
 
+
+
+        if(((BSGameState) info).computerPlayerHits == ((BSGameState) info).computerPlayerHits + 1) {
+            switch (rando()){
+                case 1:
+                    medX = medX1();
+                    medY = prevY;
+                    sleep(.75);
+
+                    game.sendAction(new BSFire(this, medX, medY));
+                    break;
+                case 2:
+                    medX = prevX;
+                    medY = medY1();
+                    sleep(.75);
+
+                    game.sendAction(new BSFire(this, medX, medY));
+                    break;
+                case 3:
+                    medX = medX2();
+                    medY = prevY;
+                    sleep(.75);
+
+                    game.sendAction(new BSFire(this, medX, medY));
+                    break;
+                case 4:
+                    medX = prevX;
+                    medY = medY2();
+                    sleep(.75);
+
+                    game.sendAction(new BSFire(this, medX, medY));
+                    break;
+            }
+        }
+
+
+
+
     }
 
 
+
+    public int rando() {
+        return (int) (Math.random() * 4);
+    }
     public int randomX() {
         return (int) (Math.random() * 10);
     }
     public int randomY() {
         return  (int) (Math.random() * 10);
+    }
+
+    public int medX1(){
+        return + 1;
+    }
+    public int medY1(){
+        return + 1;
+    }
+    public int medX2(){
+        return - 1;
+    }
+    public int medY2(){
+        return - 1;
     }
     }
 
